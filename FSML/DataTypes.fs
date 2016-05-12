a9b4bb21d70cd3e6d4d366f871d507c259a5f763
rnorm <zhang.nl@outlook.com>
2016-05-12 18:50:46 -0400

    Update DataTypes.fs

--- FSML/DataTypes.fs
+++ FSML/DataTypes.fs
@@ -1,10 +1,42 @@
-﻿module PrepareData =
+module DataTypes
 
-    open FSharp.Data
-    type csvProvider=CsvProvider<"/cd/j/bank.csv">
-    let bank = csvProvider.Load("/cd/j/bank/csv")
+    open System.IO
+    open System.Collections.Generic
+    type Variable = 
+        | Y of int
+        | X of int*double  
 
-    let getHeader someHeader = match someHeader with
-                               | None -> raise (System.Exception("csv file has no header"))
-                               | Some header -> header |> Array.toList
-    let header = bank.Headers |> getHeader
+    let parseVariable (e:string) = 
+        if e.StartsWith "+"  then Y 1
+        else if e.StartsWith "-" then Y 0
+        else let split = e.Split [|':'|]
+             X (int split.[0],double split.[1])
+
+    let private parseLine (line : string) =
+        let split = line.Split ([|' ';'\t'|], System.StringSplitOptions.RemoveEmptyEntries)
+        split |> Array.map parseVariable
+    
+    let readTable (filePath:string) = seq {
+        use sr= new StreamReader (filePath)
+        while not sr.EndOfStream do
+        yield sr.ReadLine()
+    }
+
+    let parseRow (row: array<Variable>) = 
+        if row.Length=1 
+        then (match row.[0] with 
+             |Y y -> Some y 
+             |_ -> None), Map.empty 
+        else (match row.[0] with 
+             |Y y -> Some y 
+             |_ -> None), (row.[1..] |> Seq.ofArray |> Seq.choose 
+              (fun x -> match x with     
+                        |X (feature,value) -> Some (feature,value)
+                        |_ -> None ) |> Map.ofSeq)
+
+
+    let getDict table = table|> Seq.map parseLine |> Seq.map parseRow
+
+    let getAllFeatures seqDict =
+        let featureSet = Set.empty<int>
+        seqDict |> Seq.map snd |> Seq.fold (fun acc elem -> acc + (elem |> Map.toSeq |> Seq.map fst |> Set.ofSeq)) featureSet 
