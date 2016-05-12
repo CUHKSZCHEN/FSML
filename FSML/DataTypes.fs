e91213987589cfd80289d1d36ce90b89effd4649
rnorm <zhang.nl@outlook.com>
2016-05-12 01:23:13 -0400

    test

---
+++ FSML/DataTypes.fs
@@ -0,0 +1,10 @@
+﻿module PrepareData =
+
+    open FSharp.Data
+    type csvProvider=CsvProvider<"/cd/j/bank.csv">
+    let bank = csvProvider.Load("/cd/j/bank/csv")
+
+    let getHeader someHeader = match someHeader with
+                               | None -> raise (System.Exception("csv file has no header"))
+                               | Some header -> header |> Array.toList
+    let header = bank.Headers |> getHeader
