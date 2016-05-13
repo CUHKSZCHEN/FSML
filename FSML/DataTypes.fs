85e9863a6488da5226801b1815588dcb1a0c875d
rnorm <zhang.nl@outlook.com>
2016-05-13 01:51:36 -0400

    Update DataTypes.fs

--- FSML/DataTypes.fs
+++ FSML/DataTypes.fs
@@ -1,42 +1,73 @@
 module DataTypes
 
     open System.IO
+    open System.Collections.Generic
+
+    open MathNet.Numerics
+    open MathNet.Numerics.LinearAlgebra
 
     type Variable = 
         | Y of int
         | X of int*double  
 
-    let parseVariable (e:string) = 
-        if e.StartsWith "+"  then Y 1
-        else if e.StartsWith "-" then Y 0
-        else let split = e.Split [|':'|]
-             X (int split.[0],double split.[1])
+    type Imputation = Mean=0|Median=1|Min=2|Max=3
 
-    let private parseLine (line : string) =
-        let split = line.Split ([|' ';'\t'|], System.StringSplitOptions.RemoveEmptyEntries)
-        split |> Array.map parseVariable
-    
-    let readTable (filePath:string) = seq {
-        use sr= new StreamReader (filePath)
-        while not sr.EndOfStream do
-        yield sr.ReadLine()
-    }
+    type readData (filePath: string)=
+        member this.ParseVariable (e:string) = 
+            if e.StartsWith "+"  then Y 1
+            else if e.StartsWith "-" then Y 0
+            else let split = e.Split [|':'|]
+                 X (int split.[0],double split.[1])
 
-    let parseRow (row: array<Variable>) = 
-        if row.Length=1 
-        then (match row.[0] with 
-             |Y y -> Some y 
-             |_ -> None), Map.empty 
-        else (match row.[0] with 
-             |Y y -> Some y 
-             |_ -> None), (row.[1..] |> Seq.ofArray |> Seq.choose 
-              (fun x -> match x with     
-                        |X (feature,value) -> Some (feature,value)
-                        |_ -> None ) |> Map.ofSeq)
+        member this.ParseLine (line : string) =
+            line.Split ([|' ';'\t'|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.map this.ParseVariable
+        
+        member this.ParseEntry (row: array<Variable>) = 
+            if row.Length=1 
+            then (match row.[0] with 
+                 |Y y -> y 
+                 |_ ->  raise (System.Exception("missing label")) ), Map.empty 
+            else (match row.[0] with 
+                 |Y y -> y 
+                 |_ -> raise (System.Exception("missing label")) ), (row.[1..] |> Seq.ofArray |> Seq.choose 
+                    (fun x -> match x with     
+                                |X (feature,value) -> Some (feature,value)
+                                |_ -> None ) |> Map.ofSeq)
+             
+        member this.Rows = seq {
+            use sr= new StreamReader (filePath)
+            while not sr.EndOfStream do
+            yield sr.ReadLine()} |> Seq.map this.ParseLine |> Seq.map this.ParseEntry
+
+        member this.Features =
+            this.Rows |> Seq.map snd |> Seq.fold (fun acc elem -> acc + (elem |> Map.toSeq |> Seq.map fst |> Set.ofSeq)) Set.empty<int> 
+        
+        member this.XByColumns=
+            let xByColumn = new Dictionary<int,List<Option<double>>>()
+            for f in this.Features 
+                do xByColumn.Add(f, new List<Option<double>>())
+            let rows= (this.Rows |> Seq.map snd) 
+            for row in rows do 
+                for f in this.Features do 
+                    if row.ContainsKey f then xByColumn.[f].Add (Some row.[f]) else xByColumn.[f].Add None
+            xByColumn
+        
+        member this.Impute (imputation:Imputation)= 
+            match imputation with
+            | Imputation.Mean -> 
+
+        member this.
+
+        member this.XMatrix(features) =
+            if Set.isSuperset this.Features (Set.ofList features) then
+                let xVectors = new Dictionary<int,vector<double>>()
+                for f in features do
+                    this.XByColumns.[f] |> List.map (fun x-> match x with
+                        | Some value -> value
+                        | _ -> raise (System.Exception("missing value for feature"+f)))
+            else raise System.Exception("feature not found")
 
 
-    let getDict table = table|> Seq.map parseLine |> Seq.map parseRow
 
-    let getAllFeatures seqDict =
-        let featureSet = Set.empty<int>
-        seqDict |> Seq.map snd |> Seq.fold (fun acc elem -> acc + (elem |> Map.toSeq |> Seq.map fst |> Set.ofSeq)) featureSet 
+
+
