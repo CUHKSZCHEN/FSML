c853619f352ef6143febf0bb7625472f20b784fa
rnorm <zhang.nl@outlook.com>
2016-05-13 21:25:38 -0400

    Update Program.fs

--- FSML/Program.fs
+++ FSML/Program.fs
@@ -1,8 +1,23 @@
-﻿// Learn more about F# at http://fsharp.net
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-
+open DataTypes
+open LogisticRegression
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+
+    let dat= new readData @"c://R//diabetes1.txt"
+    let seed=1
+    let folds=3
+    let datFold = new data (dat.CreateFold folds seed ,dat.Features)
+    let fold=1
+    let xTrain,yTrain= datFold.Train fold
+    let xTest,yTest= datFold.Test fold
+    let lr= new LR (xTrain, yTrain)
+    let iter=100
+    do (lr.Fit iter)
+
+    //lr.p
+    //let p= lrfit.p
     0 // return an integer exit code
+
 
