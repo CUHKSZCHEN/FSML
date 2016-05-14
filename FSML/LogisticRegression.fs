942e9efe32a1a77bec7e551ffe80e538869a32bf
rnorm <zhang.nl@outlook.com>
2016-05-14 17:54:06 -0400

    Update LogisticRegression.fs

--- FSML/LogisticRegression.fs
+++ FSML/LogisticRegression.fs
@@ -12,10 +12,13 @@ module LogisticRegression
         let rank= (sortedScore |> DenseVector.ofArray) .Ranks RankDefinition.Average
         ((rank.[0..(int)n1-1] |> Array.sum )- n1* (n1+1.0)/2.0)/n0/n1
 
+    let Loglik (p:Vector<double>) (y:Vector<double>)=
+            y*p.PointwiseLog() + (y.Negate().Add(1.0))*p.Negate().Add(1.0).PointwiseLog()
 
     type LR (x:Matrix<double>,y:Vector<double>)=
        
         let eps=1e-6
+        let xWith1= x.InsertColumn(0, DenseVector.create x.RowCount 1.0)
 
         member this.Predict (x:Vector<double>) =
             let x1= DenseVector.create (x.Count+1) 1.0
@@ -24,9 +27,7 @@ module LogisticRegression
 
         member this.Predict (x:Matrix<double>) =
             this.PredictWith1 (this.Beta, x.InsertColumn(0, DenseVector.create x.RowCount 1.0))
-
-        member private this.XWith1= x.InsertColumn(0, DenseVector.create x.RowCount 1.0)
-        
+   
         member private this.PredictWith1 (beta:Vector<double>, xWith1:Vector<double>)=xWith1 * beta 
 
         member private this.PredictWith1 (beta:Vector<double>, xWith1:Matrix<double>)=xWith1 * beta
@@ -34,16 +35,10 @@ module LogisticRegression
         member val Beta = (DenseVector.zero (x.ColumnCount+1)) with get,set
 
         member private this.Update beta  =
-            let pScore=this.PredictWith1 (beta, this.XWith1)
-            let p = pScore.Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
-            let loglikNew=this.Loglikelihood p y          
+            let p = (this.PredictWith1 (beta, xWith1) ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
+            let loglikNew=Loglik p y          
             let w= DiagonalMatrix.ofDiag (p .* p.Negate().Add(1.0))
-            let z= this.XWith1 * beta + w.Inverse() *  (y-p)
-            let betaNew= (this.XWith1.Transpose() * w *this.XWith1).Inverse() * this.XWith1.Transpose() * w*z          
-            betaNew
-
-        member this.Loglikelihood (p:Vector<double>) (y:Vector<double>)=
-            y*p.PointwiseLog() + (y.Negate().Add(1.0))*p.Negate().Add(1.0).PointwiseLog()
+            let z= xWith1 * beta + w.Inverse() *  (y-p)
+            (xWith1.Transpose() * w *xWith1).Inverse() * xWith1.Transpose() * w*z          
 
         member this.Fit k= for i in [1..k] do  this.Beta <- (this.Update this.Beta)
-          
