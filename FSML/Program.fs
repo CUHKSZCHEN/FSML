814f7d180e6bf82e81651bb801d4655e1ff13e03
rnorm <zhang.nl@outlook.com>
2016-05-15 20:49:20 -0400

    Update Program.fs

--- FSML/Program.fs
+++ FSML/Program.fs
@@ -3,7 +3,7 @@ open LogisticRegression
 [<EntryPoint>]
 let main argv = 
     // download sample data from https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/diabetes
-    let dat= new readData @"/Users/nl/Downloads/diabetes.txt"
+    let dat= new readData @"/diabetes.txt"
     let seed=1 // random seed
     let folds=3 // use 3-folds cross-validation
     let datFold = new data (dat.CreateFold folds seed ,dat.Features)
