8a53e4c0eff997fc36bc8544efe0cf7d5811a881
rnorm <zhang.nl@outlook.com>
2016-05-18 13:16:08 -0400

    Update LogisticRegression.fs

--- FSML/LogisticRegression.fs
+++ FSML/LogisticRegression.fs
@@ -77,57 +77,6 @@ module LogisticRegression
 
         member this.Fit = this.Beta <- (update reWeightedUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
 
-
-    type LASSO (x:Matrix<double>,y:Vector<double>,lambda)=
-        let n= y.Count
-        let Lambda=lambda 
-        let EPS = 1e-6
-        let checkLambda = if Lambda < 0.0 then raiseExcetion "lambda should be positive"
-        let mu,sigma = x|> getNormalizeParameter
-        let normalizedX=normalize ((M x), mu , sigma)
-        let normalizedXWith1=normalizedX.InsertColumn(0, DenseVector.create x.RowCount 1.0)
-        let reWeightedUpdate (beta:Vector<double>) =
-            let s =predictWith1 (beta, normalizedXWith1) 
-            let p = (s).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
-            let loglik= Loglik s y
-            let loss = -loglik/(double n)/2.0 + (beta.[1..].Norm(1.0))*Lambda
-            do printfn "Loglikelihood: %A\t loss: %A" loglik loss   
-            let w= DiagonalMatrix.ofDiag (p .* p.Negate().Add(1.0))
-            let z= normalizedXWith1 * beta + w.Inverse() *  (y-p)
-            (normalizedXWith1.Transpose() * w *normalizedXWith1).Inverse() * normalizedXWith1.Transpose() * w*z,loss
-
-        let coordinateDescent (betaNew:Vector<double>) (co:int)=
-            let s=predictWith1 (betaNew, normalizedXWith1)
-            let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
-            let w=p .* (p.Negate().Add(1.0))
-            let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
-            let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
-            let zNew= normalizedXWith1 * betaNew + (DiagonalMatrix.ofDiag wNew).Inverse() *  (y-pNew)
-            let denominator=wNew*(normalizedXWith1.Column(co).PointwisePower(2.0))
-            let a = ((zNew - s + normalizedXWith1.Column(co).Multiply(betaNew.At(co)) ).*wNew.*normalizedXWith1.Column(co)).Sum()
-            let b = double n*Lambda
-            if a>b then (a-b)/denominator else 
-                if a + b < 0.0 then (a+b)/denominator else 0.0
-                        
-        let cyclicCoordinateDescentUpdate (beta:Vector<double>)=
-            let betaNew,loss = reWeightedUpdate beta
-            for i in [1..beta.Count-1] do 
-                betaNew.Item(i) <- (coordinateDescent betaNew i)
-            betaNew,loss
-        
-        member val eps = 1e-6 with get,set
-        member val maxIter = 100 with get,set
-        member val minIter = 10 with get,set
-        member val Beta = (DenseVector.zero (x.ColumnCount+1)) with get,set
-        
-        member this.Fit = this.Beta <- (update cyclicCoordinateDescentUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
-
-        member this.Predict (x:Vector<double>) = 
-            predictWith1 (this.Beta, ((normalize ((V x), mu, sigma)).InsertColumn(0, DenseVector.create 1 1.0)))
-        
-        member this.Predict (x:Matrix<double>) =
-            predictWith1 (this.Beta, ((normalize ((M x), mu , sigma)).InsertColumn(0, DenseVector.create x.RowCount 1.0)))
-
      type RIDGE (x:Matrix<double>,y:Vector<double>,lambda,algorithm:string)=
         let n= y.Count
         let Lambda=lambda 
@@ -160,6 +109,10 @@ module LogisticRegression
         let coordinateDescent (betaNew:Vector<double>) (co:int)=
             let s=predictWith1 (betaNew, normalizedXWith1)
             let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
+            let loglik= Loglik s y
+            
+            let loss = -loglik/(double n)/2.0 + (betaNew.[1..].Map (fun e -> e*e)).Sum()*Lambda/2.0
+            do printfn "coordinate:%A\t Loglikelihood: %A\t loss: %A" co loglik loss   
             let w=p .* (p.Negate().Add(1.0))
             let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
             let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
@@ -170,22 +123,86 @@ module LogisticRegression
         
                         
         let cyclicCoordinateDescentUpdate (beta:Vector<double>)=
-            let betaNew,loss = reWeightedUpdate beta
-            for i in [1..beta.Count-1] do 
-                betaNew.Item(i) <- (coordinateDescent betaNew i)
-            betaNew,loss
+            for i in [0..beta.Count-1] do 
+                beta.Item(i) <- (coordinateDescent beta i)
+            let s=predictWith1 (beta, normalizedXWith1)
+            let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
+            let w=p .* (p.Negate().Add(1.0))
+            let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
+            let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
+            let zNew= normalizedXWith1 * beta + (DiagonalMatrix.ofDiag wNew).Inverse() *  (y-pNew)
+            let loss = -(wNew.*(zNew-s).*(zNew-s)).Sum()/(double n)/2.0 +  (beta.[1..].Map (fun e -> e*e)).Sum()*Lambda/2.0
+            do printfn "Loglikelihood: %A\t loss: %A" (Loglik s y)  loss
+            beta,loss
         
         new (x,y,lambda) = RIDGE(x,y,lambda,"exact")
-        member val eps = 1e-6 with get,set
+        member val eps = 1e-16 with get,set
         member val maxIter = 100 with get,set
         member val minIter = 10 with get,set
         member val Beta = (DenseVector.zero (x.ColumnCount+1)) with get,set
         
-        member this.Fit = match algorithm with
+        member this.Fit = 
+            match algorithm with
             | "cd" ->
                 this.Beta <- (update cyclicCoordinateDescentUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
             | "exact" -> this.Beta <- (update reWeightedUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
             | _ -> raiseExcetion "please choose either \"cd\" or \"exact\" algorithm"
+
+        member this.Predict (x:Vector<double>) = 
+            predictWith1 (this.Beta, ((normalize ((V x), mu, sigma)).InsertColumn(0, DenseVector.create 1 1.0)))
+        
+        member this.Predict (x:Matrix<double>) =
+            predictWith1 (this.Beta, ((normalize ((M x), mu , sigma)).InsertColumn(0, DenseVector.create x.RowCount 1.0)))
+
+
+    type LASSO (x:Matrix<double>,y:Vector<double>,lambda)=
+        let n= y.Count
+        let Lambda=lambda 
+        let EPS = 1e-6
+        let checkLambda = if Lambda < 0.0 then raiseExcetion "lambda should be positive"
+        let mu,sigma = x|> getNormalizeParameter
+        let normalizedX=normalize ((M x), mu , sigma)
+        let normalizedXWith1=normalizedX.InsertColumn(0, DenseVector.create x.RowCount 1.0)
+
+        let coordinateDescent (betaNew:Vector<double>) (co:int)=
+            let s=predictWith1 (betaNew, normalizedXWith1)
+            let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)          
+            let w=p .* (p.Negate().Add(1.0))
+            let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
+            let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
+            let zNew= s + (y-pNew)./wNew
+   
+            if co = 0 then
+                betaNew.Item(0)+wNew*(y-pNew)/(double (n*n))
+            else
+                let a = -wNew*(normalizedXWith1.Column(co).*normalizedXWith1.Column(co))/(double n)
+                let b = (normalizedXWith1.Column(co))*((y-pNew))/(double n)
+                let d=a*(b-Lambda)       
+                if betaNew.Item(co) > d then betaNew.Item(co) - d
+                else
+                    let e= a*(b+Lambda)
+                    if betaNew.Item(co) < e then betaNew.Item(co) - e else 0.0
+                     
+        let cyclicCoordinateDescentUpdate (beta:Vector<double>)=       
+            for i in [0..beta.Count-1] do 
+                beta.Item(i) <- (coordinateDescent beta i)
+            let s=predictWith1 (beta, normalizedXWith1)
+            let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
+            let w=p .* (p.Negate().Add(1.0))
+            let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
+            let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
+            let zNew= s + (y-pNew)./w
+            let approLik= -(((y-pNew))*((y-pNew)./w))/(double n)/2.0  
+            let loss =approLik + (beta.[1..].Norm(1.0))*Lambda
+            do printfn "Real Loglikelihood: %A \t Approx scaled Loglikelihood: %A\t loss: %A" (Loglik s y) approLik  loss
+            beta,loss
+        
+        member val eps = 1e-16 with get,set
+        member val maxIter = 100 with get,set
+        member val minIter = 10 with get,set
+        member val Beta = (DenseVector.zero (x.ColumnCount+1)) with get,set
+        
+        member this.Fit = this.Beta <- (update cyclicCoordinateDescentUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
 
         member this.Predict (x:Vector<double>) = 
             predictWith1 (this.Beta, ((normalize ((V x), mu, sigma)).InsertColumn(0, DenseVector.create 1 1.0)))
