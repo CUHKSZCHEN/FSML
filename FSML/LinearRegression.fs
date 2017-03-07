53e678999eee805ec3b660891b206e0ee80a6b1b
rnorm <zhang.nl@outlook.com>
2017-03-07 17:32:30 -0500

    Update LinearRegression.fs

--- FSML/LinearRegression.fs
+++ FSML/LinearRegression.fs
@@ -23,3 +23,27 @@ module LinearRegression
         // QR decomposition
         member this.Fit () = 
             this.Beta <- QRUpdate xWith1 y
+
+
+    type LinearRidge (x:Matrix<double>,y:Vector<double>,lambda)=
+
+        let n= y.Count
+        let Lambda= lambda 
+        let EPS = 1e-6
+        let checkLambda = if Lambda < 0.0 then raiseExcetion "lambda should be positive"
+        let mu,sigma = x|> getNormalizeParameter
+        let normalizedX=normalize ((M x), mu , sigma)
+        //let normalizedXWith1=normalizedX.InsertColumn(0, DenseVector.create x.RowCount 1.0)
+        let beta0 = y.Sum()/(double n)
+        member this.Predict (x:Vector<double>) = 
+            beta0 + predictWith1 (this.Beta, (normalize ((V x), mu, sigma)))
+        
+        member this.Predict (x:Matrix<double>) =
+            beta0 + predictWith1 (this.Beta, (normalize ((M x), mu , sigma)))
+
+        member val Beta = (DenseVector.zero (x.ColumnCount)) with get,set
+
+        member this.Fit () =
+            let xTilde = DenseMatrix.stack [normalizedX; DenseVector.create normalizedX.ColumnCount (sqrt(Lambda)) |> DenseMatrix.ofDiag ]
+            let yTilde =  Array.concat[(y-beta0).AsArray(); Array.zeroCreate normalizedX.ColumnCount] |> DenseVector.ofArray
+            this.Beta <- QRUpdate xTilde yTilde
