81ce085cd97cb0dd8fc00df1066e95ed437748e7
rnorm <zhang.nl@outlook.com>
2017-03-09 11:03:12 -0500

    Update LogisticRegression.fs

--- FSML/LogisticRegression.fs
+++ FSML/LogisticRegression.fs
@@ -5,11 +5,6 @@ module LogisticRegression
     open MathNet.Numerics
     open MathNet.Numerics.LinearAlgebra
     
-
-    //let predictWith1 (beta, xWith1:Matrix<double>)= xWith1 * beta
-
-
-    
     type LR (x:Matrix<double>,y:Vector<double>)=
 
         let xWith1= x.InsertColumn(0, DenseVector.create x.RowCount 1.0)
@@ -85,54 +80,45 @@ module LogisticRegression
         let checkLambda = if Lambda < 0.0 then raiseExcetion "lambda should be positive"
         let mu,sigma = x|> getNormalizeParameter
         let normalizedX=normalize ((M x), mu , sigma)
-        let normalizedXWith1=normalizedX.InsertColumn(0, DenseVector.create x.RowCount 1.0)
+        let mutable response = DenseVector.create n 0.0
+        let mutable weight= DenseVector.create n 1.0
+        let coordinateDescent (beta:Vector<double>) (co:int)=            
 
-        let coordinateDescent (betaNew:Vector<double>) (co:int)=
+            let yiTilde = beta.[0] + predictWith1 (beta.[1..], normalizedX) - beta.[co]*normalizedX.Column(co-1)
+            let z=normalizedX.Column(co-1) .*weight * (response-yiTilde) / (double n)    
+            (STO z lambda)/(weight.*normalizedX.Column(co-1)*normalizedX.Column(co-1)/(double n))
 
-            let s=predictWith1 (betaNew, normalizedXWith1)
-            let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)          
-            let w=p .* (p.Negate().Add(1.0))
-            let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
-            let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
-            let zNew= normalizedX * betaNew + (DiagonalMatrix.ofDiag wNew).Inverse() *  (y-pNew)
-            if co = 0 then
-                let g= -wNew*((y-pNew)./wNew)/(double n)
-                let h= wNew.Sum()/(double n)
-                betaNew.Item(0)-g/h
-            else
-                let sto=(normalizedX.Column(co).*wNew.*(zNew-s)).Sum()
-                let scale=(normalizedX.Column(co).*normalizedX.Column(co).*wNew).Sum()
-                if (sto>0.0 && Lambda<sto) then (sto - Lambda)/scale else 
-                    if (sto<0.0 && Lambda < -sto) then (sto + Lambda)/scale else 0.0
-//                let g1 = -(normalizedXWith1.Column(co).*((z-pNew)./wNew).*wNew).Sum()/(double n) + Lambda
-//                let h = wNew*(normalizedXWith1.Column(co).*normalizedXWith1.Column(co))/(double n)
-//                if betaNew.Item(co) - g1/h> 0.0 then betaNew.Item(co) - g1/h else
-//                    let g2 = g1 - 2.0 * Lambda
-//                    if betaNew.Item(co) - g2/h< 0.0 then betaNew.Item(co) - g2/h else 0.0
-   
         let cyclicCoordinateDescentUpdate (beta:Vector<double>)=
-            for i in [0..beta.Count-1] do 
-                beta.Item(i) <- (coordinateDescent beta i)
-            let s=predictWith1 (beta, normalizedX)
-            let p=(s ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
-            let w=p .* (p.Negate().Add(1.0))
+            let eta = beta.[0] + predictWith1 (beta.[1..], normalizedX)
+            let p = (eta ).Negate().PointwiseExp().Add(1.0).DivideByThis(1.0)
+            let w =  p.*(1.0 - p)
+
             let pNew = p.Map (fun e -> if e<EPS then 0.0 else if e+EPS>1.0 then 1.0 else e)
-            let wNew = Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
-            let zNew= normalizedX * beta + (DiagonalMatrix.ofDiag wNew).Inverse() *  (y-pNew)
-            let approLik= -(((y-pNew))*((y-pNew)./w))/(double n)/2.0  
-            let loss =approLik + (beta.[1..].Map (fun e -> abs(e))).Sum()*Lambda
-            do printfn "Real Loglikelihood: %A \t Approx scaled Loglikelihood: %A \t loss: %A" (Loglik s y) approLik  loss
-            beta,-loss
+            do weight <- Vector.map2 (fun e k -> if abs(e)<EPS then EPS else if abs(e)+EPS>1.0 then EPS else k) p w
+            do response <- eta + (y - pNew)./weight
+
+            do beta.Item(0) <- response.Sum()/(double n)
+
+            for i in [1..beta.Count-1] do 
+                beta.Item(i) <- (coordinateDescent beta i)
+
+            let eta_new = beta.[0] + predictWith1 (beta.[1..], normalizedX)
+
+            let loglik_new = Loglik eta_new y
+            let loss_new = -loglik_new/(double n)/2.0 + (beta.[1..].Map (fun e -> abs(e))).Sum()*Lambda
+            do printfn "Real Loglikelihood: %A \t loss: %A" (loglik_new) loss_new
+            beta, loss_new
 
         member val eps = 1e-16 with get,set
-        member val maxIter = 10000 with get,set
-        member val minIter = 10 with get,set
+        member val maxIter = 100 with get,set
+        member val minIter = 20 with get,set
         member val Beta = (DenseVector.zero (x.ColumnCount+1)) with get,set
         
-        member this.Fit() = this.Beta <- (update cyclicCoordinateDescentUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
+        member this.Fit () = 
+            this.Beta <- (update cyclicCoordinateDescentUpdate this.Beta this.eps 1 this.maxIter this.minIter 0.0)
 
         member this.Predict (x:Vector<double>) = 
-            predictWith1 (this.Beta, ((normalize ((V x), mu, sigma)).InsertColumn(0, DenseVector.create 1 1.0)))
+            this.Beta.[0] + predictWith1 (this.Beta.[1..], normalize ((V x), mu, sigma))
         
         member this.Predict (x:Matrix<double>) =
-            predictWith1 (this.Beta, ((normalize ((M x), mu , sigma)).InsertColumn(0, DenseVector.create x.RowCount 1.0)))
+            this.Beta.[0] + predictWith1 (this.Beta.[1..], normalize ((M x), mu, sigma))
