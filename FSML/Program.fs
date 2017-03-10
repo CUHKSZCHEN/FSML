a1363c31c9386d9d45c41b74ad2a596ac169162a
rnorm <zhang.nl@outlook.com>
2017-03-10 13:07:30 -0500

    Update Program.fs

--- FSML/Program.fs
+++ FSML/Program.fs
@@ -1,73 +1,40 @@
 open DataTypes
-open LogisticRegression
+open SVM
+open Utilities
 [<EntryPoint>]
 let main argv = 
-    // download sample data from https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/diabetes
-    let dat= new readData (@"/Downloads/diabetes.txt", "binary")
+    let dat= new readData (@"/data/binary.txt", "binary")
     let seed=1 // random seed
-    let folds=3 // use 3-folds cross-validation
-    let datFold = new data (dat.CreateFold folds seed ,dat.Features)
-    let fold=1 // use fold 1 for test
+    let folds=3 // split data into 3 folds
+    let datFold = new data (dat.CreateFold folds seed ,dat.Features) // prepare data
+    let fold=1 // use fold 1 for test and the others for train
     let xTrain,yTrain= datFold.Train fold
+    // in case to train the model using all data:
+    //let xTrain,yTrain= datFold.All 
     let xTest,yTest= datFold.Test fold
-    printfn "%A" "logistic regression:"
-    let lr= new LR (xTrain, yTrain)
-    let iter=100
-    do lr.Fit()
-    let pTrain = lr.Predict xTrain
-    let pTest = lr.Predict xTest
+    
+    // train a svm model with linear kernel 
+    let svmLinear=new SVM (xTrain, yTrain,1.0) // default kernel is linear, and the penalty parameter lambda is set to 1.0
+
+    do svmLinear.Fit()
+    let pTrain = svmLinear.Predict xTrain
+    let pTest = svmLinear.Predict xTest
     let aucTrain=AUC yTrain pTrain
     let aucTest=AUC yTest pTest
-
-    printfn "train auc: %A" aucTrain
-    printfn "test  auc: %A" aucTest
-    printfn "beta: %A" (lr.Beta.ToArray())
-
-    let lasso = new LASSO (xTrain, yTrain,0.1)
-    do lasso.Fit()
-    let pTrain = lasso.Predict xTrain
-    let pTest = lasso.Predict xTest
-    let aucTrain=AUC yTrain pTrain
-    let aucTest=AUC yTest pTest
-    printfn "%A" "lasso regression:"
+    printfn "%A" "linear SVM:"
     printfn "train auc: %A" aucTrain
     printfn "test  auc: %A" aucTest
 
-    printfn "beta: %A" (lasso.Beta.ToArray())
+    // train a svm model with rbf kernel
+    let svmRBF=new SVM (xTrain, yTrain,0.2,"rbf",1.0) // 0.2 is the penalty parameter and 1.0 is the parameter for rbf kernel
 
-
-    let ridge = new RIDGE (xTrain, yTrain,0.1)
-    do ridge.Fit()
-    let pTrain = ridge.Predict xTrain
-    let pTest = ridge.Predict xTest
-    let aucTrain=AUC yTrain pTrain
-    let aucTest=AUC yTest pTest
-    printfn "%A" "ridge regression:"
-    printfn "train auc: %A" aucTrain
-    printfn "test  auc: %A" aucTest
-    printfn "beta: %A" (ridge.Beta.ToArray())
-
-
-    let ridge1= new RIDGE (xTrain, yTrain,0.1,"cd")
-    do ridge1.Fit()
-    let pTrain = ridge1.Predict xTrain
-    let pTest = ridge1.Predict xTest
-    let aucTrain=AUC yTrain pTrain
-    let aucTest=AUC yTest pTest
-    printfn "%A" "ridge exact regression:"
-    printfn "train auc: %A" aucTrain
-    printfn "test  auc: %A" aucTest
-    printfn "beta: %A" (ridge1.Beta.ToArray())
-
-    let svm=new SVM (xTrain, yTrain,1.0)
-
-    do svm.Fit()
-    let pTrain = svm.Predict xTrain
-    let pTest = svm.Predict xTest
-    let aucTrain=AUC yTrain pTrain
-    let aucTest=AUC yTest pTest
-    printfn "%A" "SVM classification:"
-    printfn "train auc: %A" aucTrain
-    printfn "test  auc: %A" aucTest
-
+    do svmRBF.Fit()
+    let pTrainRBF = svmRBF.Predict xTrain
+    let pTestRBF = svmRBF.Predict xTest
+    let aucTrainRBF=AUC yTrain pTrainRBF
+    let aucTestRBF=AUC yTest pTestRBF
+    printfn "%A" "rbf SVM:"
+    printfn "train auc: %A" aucTrainRBF
+    printfn "test  auc: %A" aucTestRBF
+    
     0
