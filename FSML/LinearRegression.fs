f9e37dc46348f79959b2edf3a7ad6a026987c0d9
rnorm <zhang.nl@outlook.com>
2017-03-10 01:50:29 -0500

    Update LinearRegression.fs

--- FSML/LinearRegression.fs
+++ FSML/LinearRegression.fs
@@ -2,51 +2,79 @@ module LinearRegression
 
     open DataTypes
     open Utilities
+    open GLM
     open MathNet.Numerics
     open MathNet.Numerics.LinearAlgebra
 
-
-    type Lm (x:Matrix<double>,y:Vector<double>)=
-
+    type LM (xTrain:Matrix<double>,yTrain:Vector<double>)=
+        inherit model()
+        let mutable x,y=xTrain,yTrain
         let xWith1= x.InsertColumn(0, DenseVector.create x.RowCount 1.0)
-
-        member this.Predict (x:Vector<double>) = 
-            predictWith1 (this.Beta, (x.ToRowMatrix().InsertColumn(0, DenseVector.create x.Count 1.0)))
-        
-        member this.Predict (x:Matrix<double>) =
-                    predictWith1 (this.Beta, (x.InsertColumn(0, DenseVector.create x.RowCount 1.0)))
+        override this.Family = "LM"
+        override this.Penalty = "None"
 
         member val Beta = (DenseVector.zero (x.ColumnCount+1)) with get,set
 
         // chol decomposition
         //member this.Fit () = this.Beta <-  ( xWith1.TransposeThisAndMultiply(xWith1).Cholesky().Solve(xWith1.TransposeThisAndMultiply(y)) )
         // QR decomposition
-        member this.Fit () = 
+        override this.Fit () = 
             this.Beta <- QRUpdate xWith1 y
+        
+        override this.Predict(x:Vector<double>,?value:string) = 
+            let value = defaultArg value "link"
+            let link = predictLinear(this.Beta.[1..],V x) + this.Beta.[0]
+            match value with 
+            | "link" -> link
+            | "response" -> link
+            | _ -> raiseExcetion "predict either link or response"
 
-
-    type LmRidge (x:Matrix<double>,y:Vector<double>,lambda)=
+        override this.Predict(x:Matrix<double>,?value:string) = 
+            let value = defaultArg value "link"
+            let link = predictLinear(this.Beta.[1..],M x) + this.Beta.[0]
+            match value with 
+            | "link" -> link
+            | "response" -> link
+            | _ -> raiseExcetion "predict either link or response"
+ 
+    type LMRidge (x:Matrix<double>,y:Vector<double>,lambda)=
+        inherit model()
 
         let n= y.Count
         let Lambda= lambda 
         let checkLambda = if Lambda < 0.0 then raiseExcetion "lambda should be positive"
+
         let mu,sigma = x|> getNormalizeParameter
         let normalizedX=normalize ((M x), mu , sigma)
-        let beta0 = y.Sum()/(double n)
-        member this.Predict (x:Vector<double>) = 
-            beta0 + predictWith1 (this.Beta, (normalize ((V x), mu, sigma)))
-        
-        member this.Predict (x:Matrix<double>) =
-            beta0 + predictWith1 (this.Beta, (normalize ((M x), mu , sigma)))
+        let mutable beta0 = y.Sum()/(double n)
+
+        override this.Family = "LM"
+        override this.Penalty = "L2"
+
+        override this.Predict(x:Vector<double>,?value:string) = 
+            let value = defaultArg value "link"
+            let link = predictLinearScale(this.Beta,V x,mu,sigma ) + beta0
+            match value with 
+            | "link" -> link
+            | "response" -> link
+            | _ -> raiseExcetion "predict either link or response"
+
+        override this.Predict(x:Matrix<double>,?value:string) = 
+            let value = defaultArg value "link"
+            let link = predictLinearScale(this.Beta, M x, mu, sigma) + beta0
+            match value with 
+            | "link" -> link
+            | "response" -> link
+            | _ -> raiseExcetion "predict either link or response"
 
         member val Beta = (DenseVector.zero (x.ColumnCount)) with get,set
 
-        member this.Fit () =
+        override this.Fit () =
             let xTilde = DenseMatrix.stack [normalizedX; DenseVector.create normalizedX.ColumnCount (sqrt(Lambda)) |> DenseMatrix.ofDiag ]
             let yTilde =  Array.concat[(y-beta0).AsArray(); Array.zeroCreate normalizedX.ColumnCount] |> DenseVector.ofArray
             this.Beta <- QRUpdate xTilde yTilde
 
-    type LmLasso (x:Matrix<double>,y:Vector<double>,lambda)=
+    type LMLasso (x:Matrix<double>,y:Vector<double>,lambda)=
         let n= y.Count
         let Lambda=lambda 
         let checkLambda = if Lambda < 0.0 then raiseExcetion "lambda should be positive"
@@ -65,11 +93,11 @@ module LinearRegression
             let yiTilde = beta0 + predictWith1 (beta, normalizedX)
             let J = (y-yiTilde)*(y-yiTilde)/(double n)/2.0  
             let loss = J + (beta.Map (fun e -> abs(e))).Sum()*Lambda
-            do printfn "Real loss: %10.16f \t penalized Loss: %10.16f" J  loss
+            do printfn "Real loss: %A \t penalized Loss: %A" J  loss
             beta, loss
 
         member val eps = 1e-16 with get,set
-        member val maxIter = 1000 with get,set
+        member val maxIter = 100 with get,set
         member val minIter = 10 with get,set
         member val Beta = (DenseVector.zero (x.ColumnCount)) with get,set
         
