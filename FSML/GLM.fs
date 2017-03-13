f356fa9c1022a6e93c6f2796f3af38e8f2e42777
rnorm <zhang.nl@outlook.com>
2017-03-13 00:10:41 -0400

    Update GLM.fs

--- FSML/GLM.fs
+++ FSML/GLM.fs
@@ -12,17 +12,3 @@ module GLM
         abstract member Fit: unit -> unit
         abstract member Predict: Vector<double> * ?value:string -> Vector<double>
         abstract member Predict: Matrix<double> * ?value:string -> Vector<double>
-
-    //type glm (xTrain:Matrix<double>,yTrain:Vector<double>,family:string,?penlaty:string,?lambda:double)=
-    //    let Lambda = defaultArg lambda 0.0
-    //    let Penalty = defaultArg penlaty "None"
-    //    member this.model =
-    //                       match (family,Penalty) with
-    //                       | (family,penalty) when (family = "Gaussian" || family = "gaussian") && (penalty = "L2")  -> LMRidge(xTrain,yTrain,Lambda)
-    //                       | (family,penalty) when (family = "Gaussian" || family = "gaussian") && (penalty = "L1")  -> LMLasso(xTrain,yTrain,Lambda)
-    //                       | (family,penalty) when (family = "Gaussian" || family = "gaussian") && (penalty = "None")  -> LM(xTrain,yTrain)
-    //                       | (family,penalty) when (family = "binomial") && (penalty = "None")  -> LR(xTrain,yTrain)
-    //                       | (family,penalty) when (family = "binomial") && (penalty = "L2")  -> LRRidge(xTrain,yTrain,Lambda)
-    //                       | (family,penalty) when (family = "binomial") && (penalty = "L1")  -> LRLasso(xTrain,yTrain,Lambda)
-    //                       | (family,penalty) -> raiseExcetion "not implemented"
-    
